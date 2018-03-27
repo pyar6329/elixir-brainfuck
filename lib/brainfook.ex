8a7a74ee39ec2130cffbb7afe287597617a328e8
Tsukasa Arima <pyar6329@gmail.com>
2018-03-27 15:16:47 +0900

    wip

--- lib/brainfook.ex
+++ lib/brainfook.ex
@@ -30,9 +30,15 @@ defmodule Head do
 
     iex> Head.next(%Memory{cur: [1, 2, 3]})
     %Memory{prev: [1], cur: [2, 3]}
+
+    iex> Head.next(%Memory{prev: [1, 2, 3], cur: [4]})
+    %Memory{prev: [1, 2, 3, 4], cur: [0]}
   """
   def next(mem) do
     case mem.cur do
+      [head | []] ->
+        %{mem | prev: mem.prev ++ [head], cur: [0]}
+
       [head | tail] ->
         %{mem | prev: mem.prev ++ [head], cur: tail}
     end
