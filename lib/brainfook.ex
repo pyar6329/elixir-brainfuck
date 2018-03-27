2396795acf5f6036c0916162c28fcf874cd4f86a
Tsukasa Arima <pyar6329@gmail.com>
2018-03-27 15:03:11 +0900

    wip

--- lib/brainfook.ex
+++ lib/brainfook.ex
@@ -16,3 +16,22 @@ defmodule Brainfook do
     :world
   end
 end
+
+defmodule Memory do
+  defstruct prev: [], cur: [0]
+end
+
+defmodule Head do
+  import Memory
+
+  @doc ~S"""
+    iex> Head.next(%Memory{prev: [1, 2, 3], cur: [4, 5, 6]})
+    %Memory{prev: [1, 2, 3, 4], cur: [5, 6]}
+
+    iex> Head.next(%Memory{cur: [1, 2, 3]})
+    %Memory{prev: [1], cur: [2, 3]}
+  """
+  def next(mem) do
+    %Memory{prev: [1, 2, 3, 4], cur: [5, 6]}
+  end
+end
--- test/brainfook_test.exs
+++ test/brainfook_test.exs
@@ -2,3 +2,13 @@ defmodule BrainfookTest do
   use ExUnit.Case
   doctest Brainfook
 end
+
+defmodule MemoryTest do
+  use ExUnit.Case
+  doctest Memory
+end
+
+defmodule HeadTest do
+  use ExUnit.Case
+  doctest Head
+end
